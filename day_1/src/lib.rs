813d0fab6acee0005f9f55dada3059dad37c2dbd
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-01 23:03:10 -0800

    apparently its simpler than i thought.

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use anyhow::Result;
-use std::panic::resume_unwind;
 
 pub fn do_read() -> Result<Vec<i32>> {
     let mut numbers: Vec<i32> = Vec::new();
@@ -19,18 +18,14 @@ pub fn do_read() -> Result<Vec<i32>> {
 
 
 pub fn numbers2(numbers: Vec<i32>) -> Option<i32> {
-    let mut result = numbers.iter().map(
-        |x| {
-            numbers.iter().filter(|y| { x + *y == 2020 }).map(|y| {
-                x * y
-            }).next()
+    for x in numbers.iter() {
+        for y in numbers.iter(){
+            if x +y == 2020 {
+                return Some(x * y);
+            }
         }
-    ).filter(|sum| match sum {
-        None => { false }
-        Some(_) => { true }
-    });
-
-    result.next().unwrap()
+    }
+    return None
 }
 
 pub fn numbers3(numbers: Vec<i32>) -> Option<i32> {
