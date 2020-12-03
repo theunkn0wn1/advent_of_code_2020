5174782f1daf13996370bc121234ca7b1b2a5812
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-03 10:15:34 -0800

    Resolve off-by-one in solution

--- day_3/benches/day3.rs
+++ day_3/benches/day3.rs
@@ -1,13 +1,17 @@
-use criterion::{criterion_group, criterion_main, black_box};
+use criterion::{black_box, criterion_group, criterion_main};
 use day_1::read_lines;
-use day_3::{solve_p1};
+use day_3::{solve_p1, solve_p2};
 
 fn criterion_benchmark(c: &mut criterion::Criterion) {
     let line_reader = read_lines("input.txt").expect("failed to read input.");
-    let lines = line_reader.collect::<Result<Vec<_>, _>>().expect("failed to cast input to vec.");
+    let lines = line_reader
+        .collect::<Result<Vec<_>, _>>()
+        .expect("failed to cast input to vec.");
 
-    c.bench_function("day3 part 1 input.txt", |b| b.iter(|| solve_p1(&lines, 3, 1)));
-    // c.bench_function("day2 part 2 lines.txt", |b| b.iter(|| solve_p2(lines.clone())));
+    c.bench_function("day3 part 1 input.txt", |b| {
+        b.iter(|| solve_p1(&lines, 3, 1))
+    });
+    c.bench_function("day3 part 2 lines.txt", |b| b.iter(|| solve_p2(&lines)));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- day_3/src/lib.rs
+++ day_3/src/lib.rs
@@ -1,26 +1,34 @@
 #[cfg(test)]
 mod tests {
-    use day_1::read_lines;
     use crate::solve_p1;
+    use day_1::read_lines;
 
     #[test]
     fn test_part_one() {
         let line_reader = read_lines("input.txt").unwrap();
         let lines = line_reader.collect::<Result<Vec<_>, _>>().unwrap();
         println!("length of input rows: {:?}", lines.len());
-        let solution1 = solve_p1(&lines, 3, 1).unwrap();
-
+        let solution1 = solve_p1(&lines, 3, 1);
 
         assert_eq!(solution1, 167)
+    }
+
+    #[test]
+    fn test_part_one_b() {
+        let line_reader = read_lines("sample.txt").unwrap();
+        let lines = line_reader.collect::<Result<Vec<_>, _>>().unwrap();
+        println!("length of input rows: {:?}", lines.len());
+        let solution1 = solve_p1(&lines, 1, 2);
+
+        assert_eq!(solution1, 2)
     }
 }
 
 use anyhow;
 
-
-pub fn solve_p1(maze: &Vec<String>, dx: usize, dy: usize) -> anyhow::Result<usize> {
+pub fn solve_p1(maze: &Vec<String>, dx: usize, dy: usize) -> usize {
     let mut x = 0 + dx;
-    let mut y = 0 + dy;
+    // let mut y = 0 + dy;
     let mut collisions: usize = 0;
 
     let mut row_iter = maze.iter().step_by(dy);
@@ -29,24 +37,29 @@ pub fn solve_p1(maze: &Vec<String>, dx: usize, dy: usize) -> anyhow::Result<usiz
     // produce off by a lot errors down the line
 
     // println!("length of row_iter := {:?}", row_iter.len());
-    let row = row_iter.nth(dy).unwrap();
+    row_iter.next();
+    let row = row_iter.next().unwrap();
+
     // println!("accessing [{:?}][{:?}]", y, x % row.len());
     if row.chars().nth(x % row.len()).unwrap() == '#' {
         collisions += 1;
     }
     // for the rest, simply loop til EOF.
-    while let Some(row) = row_iter.next(){
+    while let Some(row) = row_iter.next() {
         x += dx;
-        y += dy;
         // println!("accessing [{:?}][{:?}]", y, x % row.len());
         if row.chars().nth(x % row.len()).unwrap() == '#' {
             collisions += 1;
         }
     }
-
-    Ok(collisions)
+    println!("collisions := {:?}", collisions);
+    collisions
 }
 
-pub fn solve_p2(maze: Vec<String> ) -> anyhow::Result<usize> {
-    unimplemented!();
+pub fn solve_p2(maze: &Vec<String>) -> usize {
+    let curves: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    curves
+        .into_iter()
+        .map(|(x, y)| crate::solve_p1(maze, x, y))
+        .product()
 }
