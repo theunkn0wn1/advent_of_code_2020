aa87444d526cab43b003fe9e1b4f62f714f9b335
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-02 17:23:22 -0800

    Almost done

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -2,20 +2,37 @@ use day_2;
 use day_1::read_lines;
 use anyhow;
 use day_2::Constraint;
-use nom::error::Error;
+use std::collections::HashMap;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()>{
+    let solution = solve()?;
+    println!("{:?}", solution);
+    Ok(())
+}
+
+fn solve() -> anyhow::Result<bool> {
     let lines = read_lines("sample.txt")?;
     let lines = lines.collect::<Result<Vec<_>, _>>()?;
+    let mut total_passing:u32 = 0;
 
     for line in lines.iter() {
         match day_2::parse_line(&line) {
-            Ok((remainder, constraint)) => {
-                println!("{:?}", constraint)
+            Ok((_remainder, constraint)) => {
+                let mut charmap: HashMap<char, u8> = HashMap::new();
+                for letter in constraint.password_body {
+                    let hash_v = charmap.entry(letter).or_insert(0);
+                    *hash_v += 1;
+                }
+
+                if let Some(count) = charmap.get(&constraint.subject) {
+                    return Ok(count >= &constraint.minimum && count <= &constraint.maximum);
+                } else {
+                    return Ok(false);
+                }
             }
             Err(e) => { anyhow::bail!("{}", e) }
         }
     }
 
-    Ok(())
+    Ok(false)
 }
