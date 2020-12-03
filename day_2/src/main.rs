387df5ad91f8aa0bb58eb770932de66e407e1273
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-02 16:48:05 -0800

    Tried to resolve lifetime issues by reading into a vec

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -4,8 +4,9 @@ use anyhow;
 
 fn main() -> anyhow::Result<()> {
     let lines = read_lines("sample.txt")?;
+    let lines = lines.collect::<Vec<_>>();
 
-    for line in lines {
+    for line in lines.iter(){
         let actual_line = line?;
         let (_, _constraint) = day_2::parse_line(&actual_line)?;
     }
