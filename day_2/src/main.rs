82eb2e4eb6b85b969ed516925ec49bfad13c41c7
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-02 16:56:40 -0800

    it compiles!

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,14 +1,18 @@
 use day_2;
 use day_1::read_lines;
 use anyhow;
+use day_2::Constraint;
+use nom::error::Error;
 
 fn main() -> anyhow::Result<()> {
     let lines = read_lines("sample.txt")?;
-    let lines = lines.collect::<Vec<_>>();
+    let lines = lines.collect::<Result<Vec<_>, _>>()?;
 
-    for line in lines.iter(){
-        let actual_line = line?;
-        let (_, _constraint) = day_2::parse_line(&actual_line)?;
+    for line in lines.iter() {
+        match day_2::parse_line(&line) {
+            Ok((remainder, constraint)) => {}
+            Err(e) => { anyhow::bail!("{}", e) }
+        }
     }
 
     Ok(())
