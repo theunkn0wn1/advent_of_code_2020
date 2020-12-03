01527328d5f9598353dee6017e01cf8020cefc84
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-02 21:24:16 -0800

    Emit both answers at once

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -7,8 +7,10 @@ fn main() -> anyhow::Result<()> {
     let line_reader = read_lines("input.txt")?;
     let lines = line_reader.collect::<Result<Vec<_>, _>>()?;
 
-    let solution = day_2::solve_p2(lines)?;
-    println!("total passing {:?}", solution);
+    let solution1 = day_2::solve_p1(lines.clone())?;
+    let solution2 = day_2::solve_p2(lines.clone())?;
+    println!("total passing p1 := {:?}", solution1);
+    println!("total passing p2 := {:?}", solution2);
     Ok(())
 }
 
