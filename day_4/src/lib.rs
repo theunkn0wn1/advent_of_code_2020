ec204830e10bf5a80837d103eabe573d3276ac49
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-07 13:47:57 -0800

    down the rabit hole of parsing the data...

--- day_4/src/lib.rs
+++ day_4/src/lib.rs
@@ -1,8 +1,9 @@
-#[allow(dead_code)]
-#[allow(unused_variables)]
-use std::fs::{File, read};
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::collections::HashMap;
+use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
 
 use itertools::Itertools;
 
@@ -19,11 +20,35 @@ mod tests {
 fn read_entries() -> anyhow::Result<()> {
     let handle = File::open("sample.txt")?;
     let line_reader = io::BufReader::new(handle).lines();
-    let lines = line_reader.collect::<Result<Vec<_>, _>>().unwrap();
+    let lines = line_reader.collect::<Result<Vec<_>, _>>()?;
 
 
     let element_iter = lines.split(|str| str == "");
-    let elements = element_iter.collect_vec();
-    println!("{:?}", elements);
+    let foo = element_iter.collect_vec();
+    for line in foo{
+        let result = process_lines(line)?;
+    }
+
+    // for element in element_iter{
+    //     let foo = element.iter().collect_vec();
+    //     println!("{:?}", foo);
+    // }
     Ok(())
+}
+
+fn process_lines(data: &[String]) -> anyhow::Result<HashMap<&str, &str>> {
+    let mut formed_data = HashMap::new();
+    println!("--------------");
+    for line in data.into_iter() {
+        println!("raw line := {:?}", line);
+        for element in line.split_ascii_whitespace(){
+            println!("raw element := {:?}", element);
+            for (key, value) in element.split(":").collect_tuple() {
+                println!("key := {:?}\t value = {:?}", key, value);
+                formed_data.insert(key, value);
+            }
+        }
+    }
+    println!("formed data := {:?}", formed_data);
+    Ok(formed_data)
 }
