9ee5d9f9f9e37f06f3d72e9fe1c461e3237b84de
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-08 11:37:09 -0800

    impl day 4

--- day_4/src/lib.rs
+++ day_4/src/lib.rs
@@ -9,7 +9,7 @@ use itertools::Itertools;
 
 #[cfg(test)]
 mod tests {
-    use crate::{read_entries, convert_entry_to_dict, solve_p1};
+    use crate::{read_entries, convert_entry_to_dict, solve_p1, solve_p2};
     use std::collections::HashMap;
 
     #[test]
@@ -44,6 +44,18 @@ mod tests {
         let validated = solve_p1("sample.txt").expect("failed!");
         assert_eq!(validated, 2);
     }
+
+    #[test]
+    fn test_p2_invalid() {
+        let validated = solve_p2("invalid.txt").expect("failed!");
+        assert_eq!(validated, 0);
+    }
+
+    #[test]
+    fn test_p2_valid() {
+        let validated = solve_p2("valid.txt").expect("failed!");
+        assert_eq!(validated, 4);
+    }
 }
 
 fn read_entries(path: &str) -> anyhow::Result<Vec<Vec<String>>> {
@@ -74,7 +86,7 @@ fn convert_entry_to_dict(data: &[String]) -> anyhow::Result<HashMap<&str, &str>>
     Ok(formed_data)
 }
 
-fn validate_p1(data: HashMap<&str, &str>) -> anyhow::Result<bool> {
+fn validate_p1(data: &HashMap<&str, &str>) -> anyhow::Result<bool> {
     if !data.contains_key("byr") {
         return Ok(false);
     }
@@ -102,13 +114,102 @@ fn validate_p1(data: HashMap<&str, &str>) -> anyhow::Result<bool> {
     Ok(true)
 }
 
+fn validate_p2(data: &HashMap<&str, &str>) -> anyhow::Result<bool> {
+    // sanity check
+    if !validate_p1(&data)? {
+        return Ok(false);
+    }
+    let byr: u16 = data.get("byr").unwrap().parse()?;
+    if byr < 1920 || byr > 2002 {
+        return Ok(false);
+    }
+    let iyr: u16 = data.get("iyr").unwrap().parse()?;
+    if iyr < 2010 || iyr > 2020 {
+        return Ok(false);
+    }
+    let eyr: u16 = data.get("eyr").unwrap().parse()?;
+    if eyr < 2020 || eyr > 2030 {
+        return Ok(false);
+    }
+    let hgt = data.get("hgt").unwrap();
+
+    if hgt.ends_with("cm") {
+        let prefix: u16 = hgt.strip_suffix("cm").unwrap().parse()?;
+        if prefix < 150 || prefix > 193 {
+            return Ok(false);
+        }
+    } else if hgt.ends_with("in") {
+        let prefix: u16 = hgt.strip_suffix("in").unwrap().parse()?;
+        if prefix < 59 || prefix > 76 {
+            return Ok(false);
+        }
+    } else {
+        return Ok(false);
+    }
+
+
+    // hcl
+    let hcl: &&str = data.get("hcl").unwrap();
+    // must be '#' prefixing six characters, therefore the length MUST be 7.
+    if hcl.len() != 7 {
+        return Ok(false);
+    }
+    // strip leading '#' prefix, if it doesn't exist its a fail.
+    if !hcl.starts_with('#') {
+        return Ok(false);
+    }
+    // this unwrap is safe as we validated the prefix existed.
+    let hcl = hcl.strip_prefix('#').unwrap();
+
+    // remainder should be a hex integer.
+    let hcl: u32 = u32::from_str_radix(hcl, 16)?;
+
+    // if we got this far, hcl is valid.
+    let ecl: &&str = data.get("ecl").unwrap();
+    if ecl.len() != 3 {
+        return Ok(false);
+    };
+    const PERMISSIBLE: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+    if !PERMISSIBLE.contains(ecl) {
+        return Ok(false);
+    }
+
+    let pid = data.get("pid").unwrap();
+
+    if pid.len() != 9 {
+        return Ok(false);
+    };
+    let _pid: u32 = pid.parse()?;
+
+
+    // if it got this far, its valid.
+    Ok(true)
+}
+
+
 pub fn solve_p1(path: &str) -> anyhow::Result<usize> {
     let entries = read_entries(path)?;
     let mut valid = 0;
     for entry in entries {
         let entry_dict = convert_entry_to_dict(&entry)?;
-        if validate_p1(entry_dict)? {
+        if validate_p1(&entry_dict)? {
             valid += 1;
+        }
+    }
+
+    Ok(valid)
+}
+
+pub fn solve_p2(path: &str) -> anyhow::Result<usize> {
+    let entries = read_entries(path)?;
+    let mut valid = 0;
+    for entry in entries {
+        let entry_dict = convert_entry_to_dict(&entry)?;
+        if validate_p1(&entry_dict)? {
+            if validate_p2(&entry_dict)?
+            {
+                valid += 1;
+            }
         }
     }
 
--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,9 +1,11 @@
-use day_4::solve_p1;
+use day_4::{solve_p1, solve_p2};
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let validated = solve_p1("input.txt")?;
-    println!("part1:= {}", validated);
+    let part_1_solution = solve_p1("input.txt")?;
+    let part_2_solution = solve_p2("input.txt")?;
+    println!("part1:= {}", part_1_solution);
+    println!("part2:= {}", part_2_solution);
 
     Ok(())
 }
