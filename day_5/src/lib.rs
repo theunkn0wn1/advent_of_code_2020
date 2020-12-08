01e0575bf4228dd4f5a95707a0201466ebfa9561
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-08 14:30:23 -0800

    parse seat column

--- day_5/src/lib.rs
+++ day_5/src/lib.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 
 #[cfg(test)]
 mod tests {
-    use crate::parse_row;
+    use crate::{parse_row, parse_seat_col};
 
     #[test]
     fn test_parse_row() {
@@ -12,10 +12,17 @@ mod tests {
         assert_eq!(parse_row("FFFBBBF"), 14);
         assert_eq!(parse_row("BBFFBBF"), 102);
     }
+
+    #[test]
+    fn test_parse_col() {
+        assert_eq!(parse_seat_col("RRR"), 7);
+        assert_eq!(parse_seat_col("RLL"), 4);
+        assert_eq!(parse_seat_col("RLR"), 5);
+    }
 }
 
 const TOTAL_ROWS: u32 = 127;
-
+const TOTAL_COLS: u32 = 7;
 
 mod identities;
 
@@ -27,7 +34,7 @@ fn parse_row(line: &str) -> u32 {
     for element in seat_iter {
         let remaining_range = range_maximum - range_minimum;
         print!("{:?}\t", element);
-            match element {
+        match element {
             RowIdentity::Front => {
                 range_maximum = range_minimum + (remaining_range) / 2;
             }
@@ -35,7 +42,30 @@ fn parse_row(line: &str) -> u32 {
                 range_minimum = range_minimum + (remaining_range) / 2;
             }
         }
-        println!("range_minimum := {}\trange_maximum := {}", range_minimum, range_maximum);
+        // println!("range_minimum := {}\trange_maximum := {}", range_minimum, range_maximum);
+    };
+
+
+    range_maximum
+}
+
+fn parse_seat_col(line: &str) -> u32 {
+    let mut range_minimum: u32 = 0;
+    let mut range_maximum = TOTAL_COLS;
+
+    let seat_iter = line.chars().map(|x| identities::SeatIdentity::new(x));
+    for element in seat_iter {
+        let remaining_range = range_maximum - range_minimum;
+        print!("{:?}\t", element);
+        match element {
+            SeatIdentity::Left => {
+                range_maximum = range_minimum + (remaining_range) / 2;
+            }
+            SeatIdentity::Right => {
+                range_minimum = range_minimum + (remaining_range) / 2;
+            }
+        }
+        // println!("range_minimum := {}\trange_maximum := {}", range_minimum, range_maximum);
     };
 
 
