92743555b907b55f0fc7cee029619b9808ee0b05
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-13 17:24:02 -0800

    Fine, lets do this imperatively.

--- day_6/src/part_1.rs
+++ day_6/src/part_1.rs
@@ -2,13 +2,25 @@ use std::collections::HashSet;
 use std::iter::FromIterator;
 use itertools::Itertools;
 
-pub(crate) fn read_groups(data: &Vec<String>) -> anyhow::Result<()>
+pub(crate) fn read_groups(data: &Vec<String>) -> anyhow::Result<Vec<HashSet<char>>>
 {
-    let groups: Vec<HashSet<&String>> = data.split(|s| s == "")
-        .map(
-            |group| HashSet::from_iter(group)
-        ).collect_vec();
+    let mut groups: Vec<HashSet<char>> = Vec::new();
+        for group in data.split(|s| s == ""){
+            let mut group_set: HashSet<char> = HashSet::new();
+            for response in group{
+                for question in response.chars(){
+                    group_set.insert(question);
+                }
+            }
+            groups.push(group_set);
+        }
 
     println!("groups := {:?}\nlen:={}", groups, groups.len());
-    Ok(())
+    Ok(groups)
+}
+
+pub(crate) fn solve_p1(groups: Vec<HashSet<char>>) -> anyhow::Result<usize>{
+    Ok(
+        groups.iter().map(|g| g.len()).sum()
+    )
 }
