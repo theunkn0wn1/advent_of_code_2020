190aa4fa212856ce3f1f940a45333a824de03913
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-13 19:14:25 -0800

    Start implementing day 2

---
+++ day_6/benches/day6.rs
@@ -0,0 +1,20 @@
+use criterion::{criterion_group, criterion_main};
+use day_6::part_1::solve_p1;
+use day_6::part_2::solve_p2;
+
+fn criterion_benchmark(c: &mut criterion::Criterion) {
+    let data = day_1::read_lines("input.txt")
+        .unwrap()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+
+    c.bench_function("day6 part 1 input.txt", |b| {
+        b.iter(|| solve_p1(&data))
+    });
+    // c.bench_function("day6 part 2 input.txt", |b| {
+    //     b.iter(|| solve_p2(&data))
+    // });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -23,3 +23,4 @@ mod tests {
     }
 }
 pub mod part_1;
+pub mod part_2;
