bc40d6b77f24a33d5470da3d1b8ebf4960dc8ff3
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-13 21:35:33 -0800

    Get the right answer (for the sample set) for p2

--- day_6/src/part_1.rs
+++ day_6/src/part_1.rs
@@ -1,24 +1,51 @@
 #![allow(dead_code)]
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub(crate) fn read_groups(data: &Vec<String>) -> Vec<HashSet<char>> {
-    let mut groups: Vec<HashSet<char>> = Vec::new();
+#[derive(Debug)]
+pub(crate) struct GroupResponse {
+    pub(crate) total_respondents: usize,
+    pub(crate) answers: HashMap<char, usize>,
+}
+impl GroupResponse {
+    pub fn new(total: usize) -> Self {
+        Self {
+            total_respondents: total,
+            answers: HashMap::with_capacity(27),
+        }
+    }
+}
+pub(crate) fn read_groups(
+    data: &Vec<String>,
+) -> Vec<GroupResponse> {
+    let mut groups: Vec<GroupResponse> = Vec::new();
     for group in data.split(|s| s == "") {
-        let mut group_set: HashSet<char> = HashSet::new();
-        for response in group {
+        let mut group_struct =
+            GroupResponse::new(group.len());
+
+        for response in group.iter() {
             for question in response.chars() {
-                group_set.insert(question);
+                if let Some(existing) =
+                    group_struct.answers.get_mut(&question)
+                {
+                    *existing += 1;
+                } else {
+                    group_struct.answers.insert(question, 1);
+                }
             }
         }
-        groups.push(group_set);
+        groups.push(group_struct);
     }
 
-    // println!("groups := {:?}\nlen:={}", groups, groups.len());
+    println!(
+        "groups := {:?}\nlen:={}",
+        groups,
+        groups.len()
+    );
     groups
 }
 
 pub fn solve_p1(lines: &Vec<String>) -> usize {
     let groups = read_groups(lines);
-    groups.iter().map(|g| g.len()).sum()
+    groups.iter().map(|g| g.answers.len()).sum()
 }
