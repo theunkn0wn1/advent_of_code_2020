1124a2b9532b1dd3a617bd8fd493d85b57d6aa65
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-13 17:30:51 -0800

    These actions are technically infalable. no need for a Result.

--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -1,12 +1,14 @@
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn test_read_groups() {
         let lines = day_1::read_lines("sample.txt")
             .expect("failed to read lines")
             .collect::<Result<Vec<_>, _>>()
             .expect("bad read.");
-        crate::part_1::read_groups(&lines).expect("read groups failed.");
+        let groups = crate::part_1::read_groups(&lines);
+        assert_eq!(groups.len(), 5)
     }
 }
 mod part_1;
--- day_6/src/part_1.rs
+++ day_6/src/part_1.rs
@@ -1,26 +1,23 @@
+#![allow(dead_code)]
+
 use std::collections::HashSet;
-use std::iter::FromIterator;
-use itertools::Itertools;
 
-pub(crate) fn read_groups(data: &Vec<String>) -> anyhow::Result<Vec<HashSet<char>>>
-{
+pub(crate) fn read_groups(data: &Vec<String>) -> Vec<HashSet<char>> {
     let mut groups: Vec<HashSet<char>> = Vec::new();
-        for group in data.split(|s| s == ""){
-            let mut group_set: HashSet<char> = HashSet::new();
-            for response in group{
-                for question in response.chars(){
-                    group_set.insert(question);
-                }
+    for group in data.split(|s| s == "") {
+        let mut group_set: HashSet<char> = HashSet::new();
+        for response in group {
+            for question in response.chars() {
+                group_set.insert(question);
             }
-            groups.push(group_set);
         }
+        groups.push(group_set);
+    }
 
-    println!("groups := {:?}\nlen:={}", groups, groups.len());
-    Ok(groups)
+    // println!("groups := {:?}\nlen:={}", groups, groups.len());
+    groups
 }
 
-pub(crate) fn solve_p1(groups: Vec<HashSet<char>>) -> anyhow::Result<usize>{
-    Ok(
-        groups.iter().map(|g| g.len()).sum()
-    )
+pub(crate) fn solve_p1(groups: Vec<HashSet<char>>) -> usize {
+    groups.iter().map(|g| g.len()).sum()
 }
