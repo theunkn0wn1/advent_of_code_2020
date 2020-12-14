e07a71e78954d92484a1dea99070e98214f804a9
Joshua Salzedo <joshuasalzedo@gmail.com>
2020-12-13 19:51:41 -0800

    Take a swing at part 2...

--- day_6/src/part_2.rs
+++ day_6/src/part_2.rs
@@ -1,37 +1,68 @@
+use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_read_groups() {
+        let lines = day_1::read_lines("sample.txt")
+            .expect("failed to read lines")
+            .collect::<Result<Vec<_>, _>>()
+            .expect("bad read.");
+        let valid_groups = crate::part_2::solve_p2(&lines);
+        assert_eq!(valid_groups, 6)
+    }
+}
+
 
 type GroupType = Vec<HashMap<char, HashSet<usize>>>;
 pub fn solve_p2(lines: &Vec<String>) -> usize {
+    let groups = read_groups(lines);
+    println!("groups := {:?}", groups);
+
+    let x = groups
+        .iter()
+        .map(|dict| dict.iter())
+        .map(|(k, v)| v.len());
+    let z = x.collect_vec();
+    println!("z := {:?}", z);
     0
 }
-pub(crate) fn read_groups(data: &Vec<String>) -> GroupType{
+pub(crate) fn read_groups(data: &Vec<String>) -> GroupType {
     let mut groups: GroupType = Vec::new();
     for group in data.split(|s| s == "") {
         // declare a hashmap to store this group's responses
         // and reserve 27 items, as we know there are only 27 questions.
-        let mut group_dict: HashMap<char, HashSet<usize>> = HashMap::with_capacity(27);
+        let mut group_dict: HashMap<char, HashSet<usize>> =
+            HashMap::with_capacity(27);
         // for each person who answered (i), take their (response)
         for (i, response) in group.iter().enumerate() {
             // and split it up into the questions they answered in the affirmative
             for question in response.chars() {
                 // If the answer is already in the dict
-                if let Some(set) = group_dict.get_mut(&question) {
+                if let Some(set) =
+                    group_dict.get_mut(&question)
+                {
                     // just insert the current respondent.
                     set.insert(i);
                 } else {
                     // otherwise we will need to create it.
                     // We know how many potential respondents in the group there is,
                     // Therefore we can reserve that many potential items up-front.
-                    let mut set = HashSet::with_capacity(group.len());
+                    let mut set =
+                        HashSet::with_capacity(group.len());
                     set.insert(i);
                     group_dict.insert(question, set);
                 }
             }
         }
         groups.push(group_dict);
-
     }
 
-    // println!("groups := {:?}\nlen:={}", groups, groups.len());
+    // println!(
+    //     "groups := {:?}\nlen:={}",
+    //     groups,
+    //     groups.len()
+    // );
     groups
 }
